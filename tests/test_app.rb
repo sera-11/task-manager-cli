2888466b400f13d20fa59564517b37f740b7dd20
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-01-23 23:03:53 +0000

    test app still not functioning

--- tests/test_app.rb
+++ tests/test_app.rb
@@ -1,23 +1,30 @@
+# test_task_manager.rb
+
 require 'minitest/autorun'
 require './app'
 
 class TestTaskManager < Minitest::Test
   def setup
-    @task_manager = TaskManager.new
+    @task_manager = Task_Manager.new  # Fix: use Task_Manager instead of TaskManager
   end
 
   def test_add_task
-    @task_manager.add_task("Sample Task", "Due/Date")
-    task = @task_manager.tasks.first
+    output = capture_output { @task_manager.add_task("Sample Task", "Due/Date") }
     assert_match "New task added to the list.", output
+    assert_equal 1, @task_manager.tasks.length
+    assert_equal "Sample Task", @task_manager.tasks.first.description
+    assert_equal "Due/Date", @task_manager.tasks.first.due_date
   end
 
   def test_remove_task
     @task_manager.add_task("Task 1", "01/30")
     @task_manager.add_task("Task 2", "02/05")
-    
+
     output = capture_output { @task_manager.remove_task(1) }
     assert_match "Task completed and removed from list.", output
+    assert_equal 1, @task_manager.tasks.length
+    assert_equal "Task 1", @task_manager.tasks.first.description
+    assert_equal "01/30", @task_manager.tasks.first.due_date
   end
-
+  
 end
