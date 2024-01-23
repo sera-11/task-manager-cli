c1098b3005ffcf0736438e662d771c5799379586
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-01-23 22:41:52 +0000

    test getting errors

--- tests/test_app.rb
+++ tests/test_app.rb
@@ -1,10 +1,23 @@
 require 'minitest/autorun'
 require './app'
 
-
 class TestTaskManager < Minitest::Test
-  def test_task_manager
-    task = Task_Manager.new
-    
+  def setup
+    @task_manager = TaskManager.new
   end
+
+  def test_add_task
+    @task_manager.add_task("Sample Task", "Due/Date")
+    task = @task_manager.tasks.first
+    assert_match "New task added to the list.", output
+  end
+
+  def test_remove_task
+    @task_manager.add_task("Task 1", "01/30")
+    @task_manager.add_task("Task 2", "02/05")
+    
+    output = capture_output { @task_manager.remove_task(1) }
+    assert_match "Task completed and removed from list.", output
+  end
+
 end
