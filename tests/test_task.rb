5a69a841eef97f18682aaae9e6433b160e343119
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-01-30 02:20:21 +0000

    fixed manager typo and created new test.. test_task.rb

--- tests/test_task.rb
+++ tests/test_task.rb
@@ -5,11 +5,19 @@ require './task_manager'
 
 class TestTask < Minitest::Test 
   def test_task 
-    task = Task.new(1,2) 
+    task = Task.new("Homework","01/30") 
     # assert something here 
+    assert_equal "Homework", task.description, "Description should match"
+    assert_equal "01/30", task.due_date, "Due date should match"
+
+
   end 
   
-  def test_task_manager task_manager = Task_Manager.new 
+  def test_task_manager 
+    task_manager = Task_Manager.new 
+
     # assert something here 
+    assert_match "New task added to the list.", task_manager.add_task("Homework", "01/30"), "Task method failed"
+    
   end 
 end
